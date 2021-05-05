e0143af602b09f336a8aa2ff45ecfa21bb3a59fb
Sufiyan Ghori <sufiyanghori@outlook.com>
2021-05-05 11:26:29 +1000

    Update main.py

--- main.py
+++ main.py
@@ -24,12 +24,12 @@ def _jwt_kms_assemtric_encryption(jwt_head, jwt_payload, aws_key_arn):
     )
     token_components["signature"] = base64.urlsafe_b64encode(response["Signature"]).decode().rstrip("=")
     return f'{token_components.get("header")}.{token_components.get("payload")}.{token_components["signature"]}'
-  
-  
- if __name__ == "__main__":
-    aws_kms_key_arn = "KMS KEY ARN"
-    public_key_file_path = "public key pem file path"
-    
+
+
+if __name__ == "__main__":
+    aws_kms_key_arn = "arn:aws:kms:ap-southeast-2:604941695572:key/20492c6e-c724-4f94-87ba-1df0c76aec60"
+    public_key_file_path = "pk.pem"
+
     header = {
         "alg": "RS256",
         "typ": "JWT"
@@ -38,13 +38,14 @@ def _jwt_kms_assemtric_encryption(jwt_head, jwt_payload, aws_key_arn):
     payload = {
         "user_name": "sufiyan-test"
     }
-    
+
     jwt_encoded = _jwt_kms_assemtric_encryption(header, payload, aws_kms_key_arn)
-    
+    print(jwt_encoded)
     key = ""
     with open(public_key_file_path, "r") as f:
         key = f.read()
-        
+
     # Decode JWT
     decoded_data = jwt.decode(jwt_encoded, key, algorithms=["RS256"])
     print(decoded_data)
+
